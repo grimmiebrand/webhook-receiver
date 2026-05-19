eba2d27508842786c6302ef40a7c495565931d75
grimmiebrand <grimmiebrand@gmail.com>
2026-05-19 12:11:44 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
